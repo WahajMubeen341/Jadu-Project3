c1c54e0899642f3142010eb42e4a3aab1fa7bc93
WahajMagazine341 <iwahajmubeen341@gmail.com>
2021-03-28 23:10:44 +0500

    user authentication added

---
+++ index.js
@@ -0,0 +1,41 @@
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+
+const addUserBtn = document.getElementById("add_user");
+
+
+const userStorage = localStorage.getItem("users");
+const JSONToUser = JSON.parse(userStorage);
+let users = JSONToUser || [];
+
+console.log(users);
+
+class User {
+  id;
+  name;
+  email;
+  password;
+
+  constructor(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
+addUserBtn.addEventListener("click", function () {
+  const user = new User(
+    Date.now(),
+    nameInput.value,
+    emailInput.value,
+    passwordInput.value
+  );
+  users.push(user);
+  const usersJson = JSON.stringify(users);
+  localStorage.setItem("users", usersJson);
+});
+
+
+ 
---
+++ login.js
@@ -0,0 +1,26 @@
+const verify_emailInput = document.getElementById("verify_email");
+const verify_passwordInput = document.getElementById("verify_password");
+
+const checkUserBtn = document.getElementById("check_member");
+const userStorage = localStorage.getItem("users");
+const JSONToUser = JSON.parse(userStorage);
+console.log(JSONToUser)
+
+var isUser = false;
+checkUserBtn.addEventListener("click", function () {
+    console.log("here");
+    console.log(verify_emailInput.value);
+    console.log(verify_passwordInput.value);
+    JSONToUser.filter((user) => {
+      if (user.email === verify_emailInput.value) {
+        if (user.password === verify_passwordInput.value) {
+         //document.location.href="./landingPage.html";
+         isUser=true;
+         console.log("true")   
+        }
+      }
+
+    });
+    isUser ? alert("user") : alert("not a user");
+  });
+ 
