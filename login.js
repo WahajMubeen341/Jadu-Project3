878122e0c33c914a6dede494b04445fea8c1c8ab
WahajMagazine341 <iwahajmubeen341@gmail.com>
2021-03-29 17:28:10 +0500

    commiting before merging

--- login.js
+++ login.js
@@ -1,32 +1,33 @@
 const verify_emailInput = document.getElementById("verify_email");
 const verify_passwordInput = document.getElementById("verify_password");
 
-
-
 const checkUserBtn = document.getElementById("check_member");
 const userStorage = localStorage.getItem("users");
 const JSONToUser = JSON.parse(userStorage);
-console.log(JSONToUser)
+console.log(JSONToUser);
 
 var isUser = false;
 checkUserBtn.addEventListener("click", function () {
-    console.log("here");
-    console.log(verify_emailInput.value);
-    console.log(verify_passwordInput.value);
-    JSONToUser.filter((user) => {
+  console.log("here");
+  console.log(verify_emailInput.value);
+  console.log(verify_passwordInput.value);
 
-        if(user.email === "" || user.password===""){
-            alert("Provide Credentials!")
-        }
-      else if (user.email === verify_emailInput.value && user.password === verify_passwordInput.value ) {
-        
-         //document.location.href="./landingPage.html";
-         isUser=true;
-         console.log("true")   
-        
-      }
-
-    });
-    isUser ? alert("Valid user") : alert("not a user");
+  JSONToUser.filter((user) => {
+    if (
+      user.email === verify_emailInput.value &&
+      user.password === verify_passwordInput.value
+    ) {
+      //document.location.href="./landingPage.html";
+      isUser = true;
+      console.log("true");
+    }
   });
- 
+
+ if(isUser){
+   //window.location.href="landingPage.html"
+  alert("Valid")
+ }
+ else{
+   alert("Invalid User")
+ }
+});
